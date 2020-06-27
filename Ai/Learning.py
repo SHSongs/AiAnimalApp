5cd996477c75a169dd40d68034c185ff50be93c7
Song <217.Songs.SH@gmail.com>
2020-06-27 17:04:20 +0900

    fix load data

--- Ai/Learning.py
+++ Ai/Learning.py
@@ -1,7 +1,8 @@
-
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2
 import os
+import numpy as np
 
 
 x_train = []
@@ -14,7 +15,8 @@ foldernames = os.listdir('../catdog/training_set/training_set')
 
 for k, folder in enumerate(foldernames):
     for file in os.listdir('../catdog/training_set/training_set' + '/'+folder):
-        x_train.append(file)
+        img = cv2.imread('../catdog/training_set/training_set' + '/' + folder + '/'+file)
+        x_train.append(img)
     y_train += [k] * len(os.listdir('../catdog/training_set/training_set' + '/'+folder))
 
 
@@ -22,5 +24,14 @@ foldernames = os.listdir('../catdog/test_set/test_set')
 
 for k, folder in enumerate(foldernames):
     for file in os.listdir('../catdog/test_set/test_set' + '/'+folder):
-        x_test.append(file)
-    y_test += [k] * len(os.listdir('../catdog/test_set/test_set' + '/'+folder))
+        img = cv2.imread('../catdog/test_set/test_set' + '/' + folder + '/'+file)
+        x_test.append(img)
+    y_test += [k] * len(os.listdir('../catdog/test_set/test_set' +'/' +folder))
+
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+
+print(x_train.shape)
