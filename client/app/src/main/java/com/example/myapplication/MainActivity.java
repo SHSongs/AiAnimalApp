34ef3777b25b211eb3e69bc94728725ac932dc68
Song <217.Songs.SH@gmail.com>
2020-06-30 20:20:45 +0900

    카메라 값 으로 추론

--- client/app/src/main/java/com/example/myapplication/MainActivity.java
+++ client/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
@@ -34,12 +35,6 @@ import com.example.myapplication.Utils.FileUtiil;
 
 public class MainActivity extends Activity{
 
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    ImageView imageView;
-    TextView textView;
-
-    Bitmap imageBitmap;
     /**
      * Float model does not need dequantization in the post-processing. Setting mean and std as 0.0f
      * and 1.0f, repectively, to bypass the normalization.
@@ -55,14 +50,6 @@ public class MainActivity extends Activity{
     /** Image size along the y axis. */
     private int imageSizeY;
 
-    /** A ByteBuffer to hold image data, to be feed into Tensorflow Lite as inputs. */
-    private ByteBuffer imgData = null;
-
-    /** An array to hold inference results, to be feed into Tensorflow Lite as outputs. */
-    private float[][] labelProbArray = null;
-
-
-
 
     /** Input image TensorBuffer. */
     private TensorImage inputImageBuffer;
@@ -74,94 +61,105 @@ public class MainActivity extends Activity{
     private TensorProcessor probabilityProcessor;
 
 
-    TextureView textureView;
-
     Interpreter tflite;
     private final Interpreter.Options tfliteOptions = new Interpreter.Options();
     /** Labels corresponding to the output of the vision model. */
-    private List<String> labels;
 
-    /** Dimensions of inputs. */
-    private static final int DIM_BATCH_SIZE = 1;
-
-    private static final int DIM_PIXEL_SIZE = 1;
-
-    static final int DIM_IMG_SIZE_X = 224;
-    static final int DIM_IMG_SIZE_Y = 224;
 
 
     private static final int IMAGE_MEAN = 128;
     private static final float IMAGE_STD = 128.0f;
-
-    /* Preallocated buffers for storing image data in. */
-    private int[] intValues = new int[DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y];
-
-    Map<String, Float> labeledProbability;
-
     private MappedByteBuffer tfliteModel;
+
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
+    TextView textView;
+
+    Bitmap imageBitmap;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AssetManager assetManager = getApplicationContext().getAssets();
 
         textView = findViewById(R.id.textView);
         imageView = findViewById(R.id.imageView);
 
         dispatchTakePictureIntent();
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            this.imageBitmap = imageBitmap;
 
-        int[] out = null;
+            predict();
+
+        }
+    }
+
+    private void predict(){
+
         try {
             tfliteModel = FileUtiil.loadMappedFile(this, "t_cat_dog.tflite");
-            tflite = new Interpreter(tfliteModel, tfliteOptions);
-
-            int imageTensorIndex = 0;
-
-            int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape();
-            imageSizeY = imageShape[1];
-            imageSizeX = imageShape[2];
-            DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
-            int probabilityTensorIndex = 0;
-            int[] probabilityShape =
-                    tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
-            DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
-            // Creates the input tensor.
-            inputImageBuffer = new TensorImage(imageDataType);
-
-            // Creates the output tensor and its processor.
-            outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
-
-
-            // Creates the post processor for the output probability.
-            // fixme 넣을까 말까?
-            probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
-
-
-            inputImageBuffer = loadImage(imageBitmap,1);
-
-            tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
-
-            out = outputProbabilityBuffer.getIntArray();
-
-            /*set text view*/
-            if (out[0] == 0) {
-                textView.setText("고양이");
-            } else if (out[0] == 1) {
-                textView.setText("강아지");
-            }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+        tflite = new Interpreter(tfliteModel, tfliteOptions);
+
+        int imageTensorIndex = 0;
+
+        int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape();
+        imageSizeY = imageShape[1];
+        imageSizeX = imageShape[2];
+        DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+        int probabilityTensorIndex = 0;
+        int[] probabilityShape =
+                tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
+        DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
 
 
 
+        int[] out = null;
 
+        // Creates the input tensor.
+        inputImageBuffer = new TensorImage(imageDataType);
+
+        // Creates the output tensor and its processor.
+        outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+
+
+        // Creates the post processor for the output probability.
+        // fixme 넣을까 말까?
+        probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
+
+
+        inputImageBuffer = loadImage(imageBitmap,1);
+
+        tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
+
+        out = outputProbabilityBuffer.getIntArray();
+
+        /*set text view*/
+        if (out[0] == 0) {
+            textView.setText("고양이");
+        } else {
+            textView.setText("강아지");
+        }
     }
 
-
-
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
     private TensorImage loadImage(Bitmap bitmap,int sensorOrientation) {
         inputImageBuffer.load(bitmap);
         int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
@@ -185,24 +183,5 @@ public class MainActivity extends Activity{
         return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-            this.imageBitmap = imageBitmap;
-        }
-    }
-
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
 }
+
