9ff2f9757abd921edff192413faf70f064ee0d75
111111022 <51100712+111111022@users.noreply.github.com>
2020-07-16 08:23:45 +0900

    기능 뺀 코드 삭제

--- AppProgrammingProject/app/src/main/java/com/example/appprogrammingproject/MainActivity.java
+++ AppProgrammingProject/app/src/main/java/com/example/appprogrammingproject/MainActivity.java
@@ -35,45 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, Loading.class);
-        startActivity(intent);
-    }
-
-//    public boolean onCreateOptionMenu(Menu menu){
-//        super.onCreateOptionsMenu(menu);
-//        MenuInflater inflater=getMenuInflater();
-//        inflater.inflate(R.menu.menu,menu);
-//        return true;
-//    }
-//
-//    public boolean onOptionsItemSelected(MenuItem item){
-//
-//
-//                AlertDialog.Builder al=new AlertDialog.Builder(this);
-//                al.setTitle("이 앱을 즐겨주세요!");
-//                al.setMessage("\uD83D\uDE0B FUNA는 심리테스트와 AI 측정을 할 수 있습니다. 심리테스트를 한 뒤에는, 심리테스트의 결과를 이용하여 최종적인 추천 결과를 얻을 수 있습니다. 충분한 심리테스트의 결과가 모이지 않으면," +
-//                        "최종 결과를 얻을 수 없으니 심리테스트를 즐겁게 즐겨주세요!!");
-//
-//                al.setPositiveButton("확인", null);
-//                al.setNegativeButton("취소",null);
-//
-//                al.show();
-//
-//                return true;
-//
-//
-//
-//
-//
-//
-//    }
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -84,17 +45,7 @@ public class MainActivity extends AppCompatActivity {
         sTest = findViewById(R.id.sTest);
         cu = findViewById(R.id.cu);
         menu=findViewById(R.id.menu);
-
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PopupMenu popupMenu=new PopupMenu(MainActivity.this,menu);
-
-                MenuInflater inflater=popupMenu.getMenuInflater();
-                inflater.inflate(R.menu.menu,popupMenu.getMenu());
-                popupMenu.show();
-            }
-        });
+        
 
 
 
